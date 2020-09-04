c31c25dd33de13395dfb09e08146da0c59856db5
Ansh <sahniansh2004@gmail.com>
2020-09-04 12:39:22 +0530

    completeAuth

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const {join} = require('path')
 const validator = require('validator');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
-console.log(bcrypt)
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 app.set('view engine','ejs');
 app.use(morgan('combined'))
 app.use(express.static(join(__dirname,'static')))
 app.use(express.json())
+app.use(cookieParser())
+let PORT = process.env.PORT || 3000
 mongoose.connect('mongodb+srv://sahninsh:8baNEERqqjA8Oizd@cluster0.nnubt.mongodb.net/this', {useNewUrlParser: true,useUnifiedTopology:true})
-    .then(() => app.listen(80))
+    .then(() => {
+        console.log('DB connected')
+        app.listen(PORT, function(){
+            console.log(`listening on port ${PORT.toString()}`)
+        })
+    })
     .catch(() => console.log('errr'))
 const userSchema =  new mongoose.Schema({
     username : {
@@ -28,6 +36,48 @@ const userSchema =  new mongoose.Schema({
     password : {
         type : String,
         required: true,
+    },
+    levels : {
+        level1 : {
+            type : Boolean,
+            default : false
+        },
+        level2 : {
+            type : Boolean,
+            default : false
+        },
+        level3 : {
+            type : Boolean,
+            default : false
+        },
+        level4 : {
+            type : Boolean,
+            default : false
+        },
+        level5 : {
+            type : Boolean,
+            default : false
+        },
+        level6 : {
+            type : Boolean,
+            default : false
+        },
+        level7 : {
+            type : Boolean,
+            default : false
+        },
+        level8 : {
+            type : Boolean,
+            default : false
+        },
+        level9 : {
+            type : Boolean,
+            default : false
+        },
+        level10 : {
+            type : Boolean,
+            default : false
+        }
     }
 }, {timestamps : true})
 let model = mongoose.model('User', userSchema)
@@ -37,29 +87,76 @@ app.route('/')
         // res.send('hello')
         res.render('index')
     })
-
-
-
+app.route('/profile')
+    .get(ensureLoggedIn, function(req,res,next){
+        model.findById((req.id).toString())
+            .then(function(item){
+                if(item){
+                    res.render('dashboard.ejs', {'user' : item})    
+                }
+                else {
+                    res.send('yo')
+                }
+                
+            })
+            .catch(function(err){
+                res.redirect('/login')
+            })
+    })
+function ensureLoggedIn(req,res,next){
+    // console.log(req.cookies.token)
+    jwt.verify(req.cookies.token, 'secretkey', function(err,decoded){
+        if(!Boolean(err)){
+            // console.log(decoded);
+            req.id = decoded.id;
+            next();
+        }
+        else {
+            // res.send('tum hee koi kharabi hein')
+            res.redirect('/login')
+        }
+    })
+    // res.send('hello')
+}
+app.get('/levels/:level', function(req,res,next){
+    res.send(req.params.level)
+})
 app.route('/login')
     .get(function(req,res,next){
         res.render('login.ejs')
     })
     .post(function(req,res){
         console.log(req.body)
-        res.send('hello')
+    
         model.find({email : req.body.email})
             .then(item => {
                 let it = item.reverse()[0]
                 console.log(item.reverse()[0])
                 bcrypt.compare(req.body.password,it.password )
-                    .then(function(item){
-                        console.log(item)
+                    .then(function(match){
+                        if(match){
+                            jwt.sign(JSON.stringify({'id' : it._id}), 'secretkey', function(err,token){
+                                if(!Boolean(err)){
+                                    console.log(token)
+                                    res.cookie('token', token)
+                                    res.redirect('/profile');
+                                    res.end();
+                                }
+                            })
+                        }
+                        else{ 
+                            res.redirect('/login')
+                        }
                     })
-                // item.reverse()
-            })
-            
+                    .catch(function(err){
+                        console.log(err)
+                    })
+            })  
     })
+app.route('/profile')
+    .get(function(req,res,next){
 
+    })
 function hashingPassword(password,cb){
     bcrypt.genSalt(8)
         .then(function(salt){
@@ -92,7 +189,8 @@ app.route('/register')
                     new model({
                         username : req.body.username,
                         email : req.body.email,
-                        password: hashedPassword
+                        password: hashedPassword,
+                        
                     })
                     .save()
                     .then(function(item){
@@ -110,5 +208,52 @@ app.route('/register')
      
     })
 
+app.get('/logout', function(req,res,next){
+    res.cookie('token', '')
+    res.redirect('/')
+})
+
+app.get('/logic', ensureLoggedIn, function(req,res,next){
+    model.findById(req.id)
+        .then(function(item){
+            let object = JSON.parse(JSON.stringify(item.levels));
+            object.level1 = true
+            
+            let array = []
+            for(let key in object){
+                // console.log(key)
+                array.push(object[key])
+            }
+            let everyItem = array.every(function(item){
+                if(item === false){
+                    return true;
+                }
+            })
+            console.log(everyItem)
+            let x = false;
+            let y;
+            for(let i =0; i < Object.values(object).length; i+=1){
+                console.log(object[(Object.values(object))[i]])
+                if(object[Object.values(object)[1]] === true){
+                    if(i < 9){
+                        y = (Object.values(object)[i+1])
+                        x = true;
+                        console.log(21)
+                        break;
+                    }
+                }
+            }
+            
+            if(Boolean(x)){
+                let route = '/levels/' + y;
+                console.log(route)
+                res.redirect(route)
+            }
+            else {
+                res.redirect('/levels/level1')
+            }
+        })   
+
+})
 // app.listen(80)
 
